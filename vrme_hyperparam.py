67f14f096e2f51825a7cdab750598a5e8ab9cb36
Marie Neubrander <mneubrander@crimson.ua.edu>
2024-07-17 17:09:00 -0500

    function to compute ATC for all files

---
+++ vrme_hyperparam.py
@@ -0,0 +1,87 @@
+import numpy as np
+import pandas as pd
+from sklearn.neighbors import NearestNeighbors, NearestCentroid
+
+def find_max_cosine(df_embeddings_2017, df_embeddings_2018, clustering, df_submission_labels):
+
+    HYPERPARAM_k_num_neighbors = 10
+
+    # This is the max possible value of cosine distance
+    # set to worst case - used to find optimal
+
+    HYPERPARAM_b_max_cosine = 2
+
+    #setting up KNN for 2018
+    neigh = NearestNeighbors(n_neighbors=HYPERPARAM_k_num_neighbors, metric = 'cosine', radius = 0.3)
+    non_anchor_embedding_2018 = np.array(df_embeddings_2018.embedding.to_list())
+    neigh.fit(non_anchor_embedding_2018)
+
+    #setting up closest centroid for anchor group 2017
+    anchor_embedding_2017 = np.array(df_embeddings_2017.embedding.tolist())
+    anchor_agg_clusters_2017 = np.array(df_embeddings_2017.agg_cluster.tolist())
+    clf = NearestCentroid()
+    clf.fit(anchor_embedding_2017, anchor_agg_clusters_2017)
+
+
+    #dictionary of all the agg clusters and the 10 KNN from 2018
+    dict_agg_cluster_matches ={}
+    for cluster_id in np.unique(clustering.labels_):
+
+        distances, indices = neigh.kneighbors([clf.centroids_[cluster_id]])
+        df_anchor_embedding = pd.concat([pd.DataFrame(data = distances.T,columns =['cos_dist']),pd.DataFrame(indices.T,columns=['indices'])],axis=1)
+
+        #get all the specified cosine distance 2018 papers
+        #tuple of (dataframe of 2018 matched papers, cosine distances)
+        dict_agg_cluster_matches[cluster_id] = (
+            df_embeddings_2018.iloc[df_anchor_embedding[df_anchor_embedding['cos_dist']<= HYPERPARAM_b_max_cosine].indices.to_list(), :],
+            df_anchor_embedding[df_anchor_embedding['cos_dist']<= HYPERPARAM_b_max_cosine].cos_dist.to_list()
+        )
+        
+    def lambda_get_2018_matches(row):
+        #get embedding matches from 2018 papers
+        #returning relevant information
+        df_clustered_papers = dict_agg_cluster_matches[row.agg_cluster]
+        lst_paper_titles = df_clustered_papers[0].title.tolist()
+        lst_paper_ids = df_clustered_papers[0].paper_id.tolist()
+        ls_paper_keywords = df_clustered_papers[0].keywords.values.tolist()
+        ls_cos_distances = df_clustered_papers[1]
+
+        return lst_paper_titles, ls_paper_keywords, lst_paper_ids, ls_cos_distances
+
+    def get_num_knn_matches(row):
+        return(len(row.titles_2018))
+
+    df_embeddings_2017[['titles_2018','keywords_2018','id_2018','cos_dist_2018']]= df_embeddings_2017.apply(lambda x: lambda_get_2018_matches(x),axis=1, result_type ='expand')
+    df_embeddings_2017['num_knn_matches'] = df_embeddings_2017.apply(lambda x: get_num_knn_matches(x),axis =1)
+
+    assert df_embeddings_2017.shape[0] == df_submission_labels[df_submission_labels['conf_year']==2017].shape[0]
+    
+    
+    df_num = pd.DataFrame(df_embeddings_2017["cos_dist_2018"].to_list())
+    
+    
+    data = []
+    found = False
+    start = 0
+    result = 0
+    
+    while found == False:
+
+        for tuning_param_cos_dist in np.linspace(start, start+0.3 ,3000):
+            input_row = {}
+            sample_number_from_2017 = df_num[df_num<=tuning_param_cos_dist].any(axis=1).sum()
+
+            input_row['sample_number_2017'] = sample_number_from_2017
+            input_row['cosine_distance'] = tuning_param_cos_dist
+            data.append(input_row)
+        # Find the smallest cosine_distance for sample_number_2017 == 490
+        distances = [entry['cosine_distance'] for entry in data if entry['sample_number_2017'] == df_embeddings_2017.shape[0]]
+
+        if distances:
+            result = min(distances)
+            found = True
+        else:
+            start = start + 0.3
+
+        
+    return(round(result, 4))
---
+++ vrme_lib.py
@@ -0,0 +1,96 @@
+### this is a very lightly modified version of the original vrme_lib file
+
+import ast
+import pandas as pd
+
+
+def print_results(name, atc, ci_lower_bound, ci_upper_bound):
+    print(f'{name} ATC: {atc:.4}')
+    print(f'{name} CI: [{ci_lower_bound:.4}, {ci_upper_bound:.4}]')
+    print(f'{name} CI range: {abs(ci_lower_bound) -abs(ci_upper_bound):.4}')
+
+
+def get_numpy (row):
+    return ast.literal_eval(row.embedding)
+
+
+def load_data(path, embedding_path = 'data/original/df_embeddings.csv'):
+    embedding_path = embedding_path
+    submission_path = path+'df_submission_rating.csv'
+
+    df_embeddings = pd.read_csv(embedding_path).T
+    df_embeddings.columns=df_embeddings.iloc[0]
+    df_embeddings = df_embeddings.iloc[1: , :]
+
+    df_embeddings['embedding'] = df_embeddings.apply(lambda x: get_numpy(x), axis =1)
+
+    df_submissions = pd.read_csv(submission_path)
+    df_submission_labels = df_submissions[['id','title','conf_year','keywords','AVG_rating']]
+
+    df_embeddings_2017 = df_embeddings.merge(df_submission_labels[df_submission_labels['conf_year']==2017], left_on='paper_id',right_on='id')
+    df_embeddings_2018 = df_embeddings.merge(df_submission_labels[df_submission_labels['conf_year']==2018], left_on='paper_id',right_on='id')
+    assert df_submission_labels[df_submission_labels['conf_year']==2017].shape[0] == df_embeddings_2017.shape[0]
+
+
+    return df_embeddings, df_submissions, df_submission_labels, df_embeddings_2017, df_embeddings_2018
+
+
+# ==== SPSM utils ================================================================================
+
+def mean_predictions(dummy, y_pred):
+    """
+    Helpful for error diagnosing. Returns the mean of the predicted values
+    Args:
+        - dummy : we need this arg so that this function looks the same as
+        sklearn error metrics that take inputs y_true, y_pred
+        - y_pred : np.array
+    """
+    return np.mean(y_pred)
+
+def calibration_rmse(y_true, y_pred):
+    """
+    Calculates calibration root mean squared error (RMSE).
+    Calibration is the extent to which a model's probabilistic predictions match their
+    corresponding empirical frequencies.
+    See Nguyen and O'Connor 2015's introduction and definitions
+    https://www.emnlp2015.org/proceedings/EMNLP/pdf/EMNLP182.pdf
+    """
+    prob_true, prob_pred = calibration_curve(y_true, y_pred, n_bins=10, strategy='uniform')
+    rms = mean_squared_error(prob_true, prob_pred, squared=False) #False returns RMSE vs MSE
+    return rms
+
+def mean_truth(y_true, dummy):
+    """
+    Helpful for error diagnosing. Returns the mean of the true values
+    Args:
+        - y_true : np.array
+        - dummy : we need this arg so that this function looks the same as
+        sklearn error metrics that take inputs y_true, y_pred
+    """
+    return np.mean(y_true)
+
+def strip_char(row):
+    word_list = ast.literal_eval(row.keywords)
+    s = ''
+    for word in word_list:
+        s += " " + word.replace(' ','_').replace('-','_').lower()
+    return s
+
+
+# ==== VRM-E utils ===============================================================================
+
+def lambda_get_match_potential_outcomes(row, df_embeddings_2018): # Eqn 7
+    paper_ids = row.id_2018
+    big_m = len(row.id_2018)
+    if big_m == 0:
+        return None
+    ratings = [df_embeddings_2018[df_embeddings_2018['paper_id'] == paper_id].AVG_rating.values[0] for paper_id in paper_ids]
+    return sum(ratings)/big_m
+
+# ==== Agreement utils ===========================================================================
+def print_ttest(other_method_name, ttest):
+    print(f'T-Test: {other_method_name} vs VRM-E')
+    print(f'statistic: {ttest.statistic:.4}')
+    print(f'p-value: {ttest.pvalue}')
+    print()
+
